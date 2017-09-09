37d10601bd9a6367032647f347f27d819a128004
David Orbelian <david.orbelian@gmail.com>
2017-09-09 15:57:31 +0400

    Code cleanup.

--- src/MicroNetCore.Rest/Extensions/ConfigurationExtensions.cs
+++ src/MicroNetCore.Rest/Extensions/ConfigurationExtensions.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Reflection.Emit;
-using Humanizer;
 using MicroNetCore.Rest.Hypermedia.Extensions;
 using MicroNetCore.Rest.Services;
 using Microsoft.Extensions.DependencyInjection;
@@ -17,7 +13,7 @@ namespace MicroNetCore.Rest.Extensions
             IEnumerable<Type> restTypes)
         {
             services.AddCustomMvc()
-                .ConfigureApplicationPartManager(apm => apm.ApplicationParts.Add(GetRestApplicationPart(restTypes)));
+                .ConfigureApplicationPartManager(apm => apm.ApplicationParts.Add(new RestApplicationPart(restTypes)));
 
             services.AddTransient(typeof(IRestService<>), typeof(RestService<>));
             services.AddRestHypermedia();
@@ -39,49 +35,6 @@ namespace MicroNetCore.Rest.Extensions
             builder.AddXmlDataContractSerializerFormatters();
 
             return builder;
-        }
-
-        private static RestApplicationPart GetRestApplicationPart(IEnumerable<Type> types)
-        {
-            var moduleBuilder = CreateModuleBuilder();
-
-            var controllerTypes = types.Select(t => CreateControllerType(moduleBuilder, t));
-            var applicationPart = new RestApplicationPart(controllerTypes);
-
-            return applicationPart;
-        }
-
-        private static ModuleBuilder CreateModuleBuilder()
-        {
-            var assemblyName = new AssemblyName(Guid.NewGuid().ToString());
-            var assembly = AssemblyBuilder.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.Run);
-
-            return assembly.DefineDynamicModule(Guid.NewGuid().ToString());
-        }
-
-        private static Type CreateControllerType(ModuleBuilder moduleBuilder, Type modelType)
-        {
-            const TypeAttributes typeAttributes = TypeAttributes.Public | TypeAttributes.Class | TypeAttributes.Sealed;
-
-            var typeName = modelType.GetControllerName();
-            var parentType = modelType.GetControllerType();
-
-            var typeBuidler = moduleBuilder
-                .DefineType(typeName, typeAttributes, parentType)
-                .WithPassThroughConstructors(parentType);
-
-            return typeBuidler.CreateType();
-        }
-
-        // ReSharper disable once SuggestBaseTypeForParameter
-        private static string GetControllerName(this Type modelType)
-        {
-            return $"{modelType.Name.Pluralize()}Controller";
-        }
-
-        private static Type GetControllerType(this Type modelType)
-        {
-            return typeof(RestController<>).MakeGenericType(modelType);
         }
 
         #endregion
--- src/MicroNetCore.Rest/RestApplicationPart.cs
+++ src/MicroNetCore.Rest/RestApplicationPart.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Reflection.Emit;
+using Humanizer;
+using MicroNetCore.Rest.Extensions;
 using Microsoft.AspNetCore.Mvc.ApplicationParts;
 
 namespace MicroNetCore.Rest
@@ -12,10 +15,29 @@ namespace MicroNetCore.Rest
 
         public RestApplicationPart(IEnumerable<Type> types)
         {
-            Types = types.Select(t => t.GetTypeInfo());
+            var assemblyName = new AssemblyName(Guid.NewGuid().ToString());
+            var assembly = AssemblyBuilder.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.Run);
+
+            var moduleBuilder = assembly.DefineDynamicModule(Guid.NewGuid().ToString());
+
+            Types = types.Select(t => CreateControllerType(moduleBuilder, t).GetTypeInfo());
         }
 
         public override string Name => ApplicationPartName;
         public IEnumerable<TypeInfo> Types { get; }
+
+        private static Type CreateControllerType(ModuleBuilder moduleBuilder, Type modelType)
+        {
+            const TypeAttributes typeAttributes = TypeAttributes.Public | TypeAttributes.Class | TypeAttributes.Sealed;
+
+            var typeName = $"{modelType.Name.Pluralize()}Controller";
+            var parentType = typeof(RestController<>).MakeGenericType(modelType);
+
+            var typeBuidler = moduleBuilder
+                .DefineType(typeName, typeAttributes, parentType)
+                .WithPassThroughConstructors(parentType);
+
+            return typeBuidler.CreateType();
+        }
     }
 }
